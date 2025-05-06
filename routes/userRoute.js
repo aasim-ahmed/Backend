c2da7da2883ae4af3e15e5fb2a3e4134e4375bb7
aasim-ahmed <theaasim22@gmail.com>
2025-05-06 18:02:06 +0530

    this is updated code

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,7 @@ const userRoutes = express.Router();
 
 userRoutes.post("/createUser", userController.createUser);
 userRoutes.get("/getAllUsers", userController.getAllUsers);
+userRoutes.get("/login", userController.loginUser);
 userRoutes.get("/:id", userController.getUser);
 userRoutes.put("/:id", userController.updateUser);
 userRoutes.delete("/:id", userController.deleteUser);
