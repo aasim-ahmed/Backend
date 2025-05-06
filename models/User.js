d69f8f496c0b9ca0bb7f4cad52d836788628c26a
aasim-ahmed <theaasim22@gmail.com>
2025-05-06 18:11:37 +0530

    this is updated code

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    number: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    organization: {
+      type: String,
+      trim: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'recruiter', 'technical_expert', 'candidate'],
+      default: 'candidate',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
