e0e9664fa1616167219a252280644b74081e6b3e
aasim-ahmed <theaasim22@gmail.com>
2025-05-07 14:49:45 +0530

    this is updated code

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import questionController from "../controllers/questionController.js";
+import auth from "../middleware/authMiddleware.js";
 
 const questionRoutes = express.Router();
 
@@ -9,8 +10,8 @@ questionRoutes.get("/search", questionController.searchQuestions);
 questionRoutes.get("/:id", questionController.getQuestionById);
 
 // private routes
-questionRoutes.post("/", questionController.createQuestion);
-questionRoutes.put("/:id", questionController.updateQuestion);
-questionRoutes.delete("/:id", questionController.deleteQuestion);
+questionRoutes.post("/", auth, questionController.createQuestion);
+questionRoutes.put("/:id",auth, questionController.updateQuestion);
+questionRoutes.delete("/:id",auth, questionController.deleteQuestion);
 
 export default questionRoutes;
