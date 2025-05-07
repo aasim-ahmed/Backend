620bfcf8bc25b1300f24adc902178000d0f93d21
aasim-ahmed <theaasim22@gmail.com>
2025-05-07 14:08:12 +0530

    this is updated code

--- routes/questionTypeRoute.js
+++ routes/questionTypeRoute.js
@@ -3,10 +3,14 @@ import questionTypeController from "../controllers/questionTypeController.js";
 
 const questionTypeRoutes = express.Router();
 
-questionTypeRoutes.post("/createQuestionType", questionTypeController.createQuestionType);
+// public routes
 questionTypeRoutes.get("/getAllQuestionTypes", questionTypeController.getAllQuestionTypes);
+questionTypeRoutes.get("/search", questionTypeController.searchQuestionTypes);
 questionTypeRoutes.get("/getQuestionType/:id", questionTypeController.getQuestionType);
-questionTypeRoutes.put("/updateQuestionType/:id", questionTypeController.updateQuestionType);
-questionTypeRoutes.delete("/deleteQuestionType/:id", questionTypeController.deleteQuestionType);
 
+
+// Protected routes
+questionTypeRoutes.put("/updateQuestionType/:id", questionTypeController.updateQuestionType);
+questionTypeRoutes.post("/createQuestionType", questionTypeController.createQuestionType);
+questionTypeRoutes.delete("/deleteQuestionType/:id", questionTypeController.deleteQuestionType);
 export default questionTypeRoutes;
