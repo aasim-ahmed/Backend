bcf67e8aace2ac13097521814fbd8c93bf30d23a
aasim-ahmed <theaasim22@gmail.com>
2025-05-07 18:28:43 +0530

    this is updated code

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -1,1 +1,19 @@
 import express from "express";
+import reviewController from "../controllers/reviewController.js";
+import auth from "../middleware/authMiddleware.js"
+
+const reviewRoutes = express.Router();
+
+// Submit a review (after completing assessment)
+reviewRoutes.post('/', reviewController.submitReview);
+
+// Get all reviews (with filters by assessment, rating, time)
+reviewRoutes.get('/', auth, reviewController.getAllReviews);
+
+// Get single review by ID
+reviewRoutes.get('/:id', auth, reviewController.getReviewById);
+
+// Delete review (admin/recruiter only)
+reviewRoutes.delete('/:id', auth, reviewController.deleteReview);
+
+export default reviewRoutes;
