a4933629c2713d7af741e2345e279c35e83fc07c
aasim-ahmed <theaasim22@gmail.com>
2025-05-08 18:04:40 +0530

    this is updated code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,7 +13,9 @@ const auth = async (req, res, next) => {
   
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const user = await User.findById(decoded.id).select('-password');
+    
+      const user = await User.findById(decoded.userId)
+      
   
       if (!user) {
         return res.status(401).json({ error: 'Unauthorized: User not found' });
